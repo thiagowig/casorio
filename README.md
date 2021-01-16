fca7c5b49444604e76d7afe81d8d2991b289268f
Thiago Fonseca <dev.thiago@gmail.com>
2021-01-15 22:41:27 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Casamento do século
---
+++ platform/src/main/kotlin/br/com/ithiago/casorio/platform/Application.kt
@@ -0,0 +1,11 @@
+package br.com.ithiago.casorio.platform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlatformApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlatformApplication>(*args)
+}
